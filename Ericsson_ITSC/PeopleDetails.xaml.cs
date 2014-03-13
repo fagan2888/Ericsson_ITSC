43bc4b961427470ba567e00bb0250db6157c9724
sefayilmaz <sefayilmaz91@gmail.com>
2014-03-13 09:47:22 +0200

    Merge branch 'master' of https://github.com/hburak/Ericsson_ITSC.git

---
+++ Ericsson_ITSC/PeopleDetails.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Ericsson_ITSC_Library;
+
+namespace Ericsson_ITSC
+{
+    public partial class PeopleDetails : PhoneApplicationPage
+    {
+        public static Contacts 
+        public PeopleDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
